110a5831964515ae6c3dfd1f5c0b662ef7661e0f
Aizhan-15 <aizhanurman@gmail.com>
2022-09-09 16:51:07 +0600

    done

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,15 +1,10 @@
 package lang.print.gaps.task5;
 
-import java.util.Scanner;
-
 public class TimeConvertor {
     public static void main (String[] args) {
-
             int min,sec;
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter Minutes");
-            min=sc.nextInt();
-            sec=min*60;
+            min=4;
+        sec=min*60;
             System.out.println("Seconds: "+sec);
         }
     }
